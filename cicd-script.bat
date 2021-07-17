016cf6ad3b7aa0f1361b4528e845b2a455ee0287
Shuchita009 <46736238+Shuchita009@users.noreply.github.com>
2021-07-17 23:48:38 +0530

    Update cicd-script.bat

--- cicd-script.bat
+++ cicd-script.bat
@@ -53,8 +53,7 @@ set "$appendDateTimeInFolderName=true"
 set "$assigneeUser=557058:XXXXXXXX-XXXX-XXXX-XXXX-da60ef125371"
 
 :: Fully-qualitified name of the test result file
-#set "$resultPath=@\"C:/Test results/junit.xml\"
-set "$resultPath=@\C:/Users/spare/.jenkins/workspace/JavaFreeStyleGoogle/target/cucumber-reports/json-reports/CucumberTestReport.json"
+set "$resultPath=C:/Users/spare/.jenkins/workspace/JavaFreeStyleGoogle/target/cucumber-reports/json-reports/CucumberTestReport.json"
 
 :: =========================================
 ::  Create an automation task, run it, send test results to Zephyr.
