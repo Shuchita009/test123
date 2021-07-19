6df94e9add233361728a8bfeeb668a8cd28a0fd2
Shuchita009 <46736238+Shuchita009@users.noreply.github.com>
2021-07-19 14:40:09 +0530

    Update cicd-script.bat

--- cicd-script.bat
+++ cicd-script.bat
@@ -56,7 +56,7 @@ set "$assigneeUser=60f4f41bf026ab00700ab52c"
 set "$resultPath=@\"C:/Users/spare/.jenkins/workspace/JavaFreeStyleGoogle/target/cucumber-reports/json-reports/CucumberTestReport.json""
 
 :: Set Mandatory
-set "$mandatoryFields="{\"issueType\":\"Test\",\"project\":\"Test\",\"reporter\":{\"label\":\"shuchita pare\",\"name\":\"shuchita pare\",\"id\": \"60f4f41bf026ab00700ab52c\"}}"
+set "$mandatoryFields={\r\n   \"project\":\"Test\",\r\n   \"issueType\":\"Test\",\r\n   \"reporter\":{\r\n      \"label\":\"shuchita pare\",\r\n      \"name\":\"shuchita pare\",\r\n      \"id\":\"60f4f41bf026ab00700ab52c\"\r\n   }\r\n}"
 
 :: =========================================
 ::  Create an automation task, run it, send test results to Zephyr.
