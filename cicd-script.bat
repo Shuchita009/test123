637a6f99a182b412e46af7fb51bb4d66ae81b716
Shuchita009 <46736238+Shuchita009@users.noreply.github.com>
2021-07-20 11:47:34 +0530

    Update cicd-script.bat

--- cicd-script.bat
+++ cicd-script.bat
@@ -63,3 +63,19 @@ set "$resultPath=@\"C:/Users/spare/.jenkins/workspace/JavaFreeStyleGoogle/target
 :: =========================================
 echo "Creating and running an automation task ..."
 curl -v -X POST https://prod-vortexapi.zephyr4jiracloud.com/api/v1/automation/job/saveAndExecute  -H "Content-Type: multipart/form-data" -H "Content-Type: application/json" -H "accessKey: %$accessKey%" -H "jwt: %$finaljwt%" -F "jobName=%$taskName%" -F "jobDescription=%$taskDescription%" -F "automationFramework=%$automationFramework%" -F "projectKey=%$projectKey%" -F "versionName=%$versionName%" -F "cycleName=%$cycleName%" -F "createNewCycle=%$createNewCycle%" -F "appendDateTimeInCycleName=%$appendDateTimeInCycleName%" -F "folderName=%$folderName%" -F "createNewFolder=%$createNewFolder%" -F "appendDateTimeInFolderName=%$appendDateTimeInFolderName%" -F "assigneeUser=%$assigneeUser%" -F "mandatoryFields=%$mandatoryFields%" -F "file=%$resultPath%"
+
+:: =========================================
+:: Update the properties of automation task and run it, send test results to Zephyr.
+::
+:: The code below uses "task properties" values defined in lines 38-56.
+:: Update them to change task properties.
+::
+:: To run the code below, first comment out the lines 62-63 above that create your automation task,
+:: and then uncomment in the lines below.
+:: =========================================
+
+:: Id of your automation task. You get it after the task is created.
+:: set "$taskId=XXXXXXXXXXXXXXXXXXXX42E96E8863BF7EEE44BDB95FF763DA3E9C3BC4776886"
+
+:: echo "Updating and running the automation task"
+:: curl -v -X PUT https://prod-vortexapi.zephyr4jiracloud.com/api/v1/automation/job/updateAndExecute -H "Content-Type: multipart/form-data" -H "Content-Type: application/json" -H "accessKey: %$accessKey%" -H "jwt: %$finaljwt%" -F "jobId=%$taskId%" -F "jobName=%$taskName%" -F "jobDescription=%$taskDescription%" -F "automationFramework=%$automationFramework%" -F "versionName=%$versionName%" -F "cycleName=%$cycleName%" -F "createNewCycle=%$createNewCycle%" -F "appendDateTimeInCycleName=%$appendDateTimeInCycleName%" -F "folderName=%$folderName%" -F "createNewFolder=%$createNewFolder%" -F "appendDateTimeInFolderName=%$appendDateTimeInFolderName%" -F "assigneeUser=%$assigneeUser%" -F "file=%$resultPath%"
