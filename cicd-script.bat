70dcb494238c299a3c8207fd6a48c920667179c0
Shuchita009 <46736238+Shuchita009@users.noreply.github.com>
2021-08-06 11:38:28 +0530

    Update cicd-script.bat

--- cicd-script.bat
+++ cicd-script.bat
@@ -1,81 +1,101 @@
-:: =========================================
-::  This .bat file demonstrates how to create or update an automation task in Zephyr for Jira Cloud, run this task, and publish test results to Zephyr.
-::  Author: SmartBear Software
-:: =========================================
 
-:: =========================================
-::  Zephyr base URL.
-::   DON'T CHANGE THE CONSTANT BELOW. KEEP IT AS IT IS.
-:: =========================================
-set "$zephyrBaseUrl=https://prod-api.zephyr4jiracloud.com/connect" 
+######################################################################
+#  This .sh file demonstrates how to create or update an automation task in Zephyr for Jira Cloud, run this task, and publish test results to Zephyr.
+#  Author: SmartBear Software
+######################################################################
 
-:: =========================================
-::  Access and secret keys, and user id needed for connection to Zephyr for Jira. 
-::  Replace the constants below with values relevant to your project and account.
-:: =========================================
-:: The accessKey and secretKey to access your project. You can find them in your Jira project: Zephyr > API Keys.
-set "$accessKey=ODhmNjZkNjQtZjM4NS0zYjM0LWI0OWYtMjU3ZTdlMjQyN2EzIDYwZjRmNDFiZjAyNmFiMDA3MDBhYjUyYyBVU0VSX0RFRkFVTFRfTkFNRQ"
-set "$secretKey=7-HKGkBijaA4yaMdW6bG40XRVC11FN1u5CiCNah46z4"
-:: Id of the user who will create the automation task. You can find it in Jira.
-set "$accountId=557058:60f4f41bf026ab00700ab52c"
+######################################################################
+#  Zephyr base URL.
+#  DON'T CHANGE THE CONSTANT BELOW. KEEP IT AS IT IS.
+######################################################################
+zephyrBaseUrl="https://prod-api.zephyr4jiracloud.com/connect"
 
-:: =========================================
-::  Create a JSON Web Token  (required to access Zephyr for Jira).
-::  Keep this section as it is.
-:: =========================================
-echo "Generating a JSON Web Token ..."
-for /F "tokens=*" %%i in (' curl -s POST -H "Content-Type: application/json" --data "{ \"accessKey\":\"%$accessKey%\", \"secretKey\":\"%$secretKey%\",\"accountId\":\"%$accountId%\",\"zephyrBaseUrl\": \"%$zephyrBaseUrl%\",\"expirationTime\":360000}" "https://prod-vortexapi.zephyr4jiracloud.com/api/v1/jwt/generate" ')  do set jwtgenerated=%%i
-set "$finaljwt=%jwtgenerated%"
-echo %$finaljwt%
+#######################################################################
+#  Access and secret keys, and user id needed for connection to Zephyr for Jira. 
+#  Replace the constants below with values relevant to your project and account.
+#######################################################################
 
-:: =========================================
-::  Define properties of the automation task.
-::  Replace the values below with data relevant to your project.
-:: =========================================
+# The accessKey and secretKey to access your project. You can find them in your Jira project: Zephyr > API Keys.
+accessKey="ODhmNjZkNjQtZjM4NS0zYjM0LWI0OWYtMjU3ZTdlMjQyN2EzIDYwZjRmNDFiZjAyNmFiMDA3MDBhYjUyYyBVU0VSX0RFRkFVTFRfTkFNRQ"
+secretKey="7-HKGkBijaA4yaMdW6bG40XRVC11FN1u5CiCNah46z4"
+# Id of the user who will create the automation task. You can find it in Jira.
+accountId="557058:60f4f41bf026ab00700ab52c"   
 
-:: Task info
-set "$taskName=Test"
-set "$taskDescription=Test"
-set "$automationFramework=CUCUMBER"
-set "$projectKey=TEST"
-set "$versionName=UNSCHEDULED"
+#######################################################################
+#  Create a JSON Web Token  (required to access Zephyr for Jira).
+#  Keep this section as it is.
+#######################################################################
+echo "Generating a JSOM Web Token ... \n"
+curl -o headers -s -d '{  "accessKey": "'"$accessKey"'"  , "secretKey": "'"$secretKey"'" ,"accountId": "'"$accountId"'","zephyrBaseUrl": "'"$zephyrBaseUrl"'","expirationTime":360000}' -H "Content-Type: application/json" -X POST https://prod-vortexapi.zephyr4jiracloud.com/api/v1/jwt/generate
+jwt="$(cat  headers | head -n 1)"
+echo "The generated token: \n"
+echo $jwt
 
-:: Cycle info
-set "$cycleName=Regression"
-set "$createNewCycle=true"
-set "$appendDateTimeInCycleName=true"
+#######################################################################
+#  Define properties of the automation task.
+#  Replace the values below with data relevant to your project.
+#######################################################################
 
-:: Folder info
-set "$folderName="Chrome""
-set "$createNewFolder=true"
-set "$appendDateTimeInFolderName=true"
-set "$assigneeUser=60f4f41bf026ab00700ab52c"
+# Task info
+taskName="Test"                     
+taskDescription="Test"
+automationFramework="CUCUMBER"  
+projectKey="TEST"
+versionName="UNSCHEDULED"
 
-:: Set Mandatory
-set "$mandatoryFields={"reporter":{"label":"shuchitapare","name":"shuchitapare","id":"60f4f41bf026ab00700ab52c"}}"
+# Cycle info
+cycleName="Regression"
+createNewCycle="true"
+appendDateTimeInCycleName="true"
 
-:: Fully-qualitified name of the test result file
-set "$resultPath=@\"C:/Users/spare/.jenkins/workspace/JavaFreeStyleGoogle/target/cucumber-reports/json-reports/CucumberTestReport.json"\"
+# Folder info
+folderName="RegressionTests"
+createNewFolder="true"
+appendDateTimeInFolderName="true"
+assigneeUser="557058:60f4f41bf026ab00700ab52c"
 
-:: =========================================
-::  Create an automation task, run it, send test results to Zephyr.
-::  Keep this section as it is.
-:: =========================================
+#Set Mandatory
+mandatoryFields={"reporter":{"label":"shuchitapare","name":"shuchitapare","id":"557058:60f4f41bf026ab00700ab52c"}}
+
+echo "curr dir: $pwd"
+# Name of the test result file
+resultPath="$pwd/output/json-reports/CucumberTestReport.json"
+
+#######################################################################
+#  Create an automation task, run it, send test results to Zephyr.
+#  Keep this section as it is.
+#######################################################################
 echo "Creating and running an automation task ..."
-curl -v -X POST https://prod-vortexapi.zephyr4jiracloud.com/api/v1/automation/job/saveAndExecute  -H "Content-Type: multipart/form-data" -H "Content-Type: application/json" -H "accessKey: %$accessKey%" -H "jwt: %$finaljwt%" -F "jobName=%$taskName%" -F "jobDescription=%$taskDescription%" -F "automationFramework=%$automationFramework%" -F "projectKey=%$projectKey%" -F "versionName=%$versionName%" -F "cycleName=%$cycleName%" -F "createNewCycle=%$createNewCycle%" -F "appendDateTimeInCycleName=%$appendDateTimeInCycleName%" -F "folderName=%$folderName%" -F "createNewFolder=%$createNewFolder%" -F "appendDateTimeInFolderName=%$appendDateTimeInFolderName%" -F "assigneeUser=%$assigneeUser%" -F "mandatoryFields=%$mandatoryFields%" -F "file=%$resultPath%"
+curl -o headers -s -v -H "accessKey: $accessKey" -H "jwt: $jwt" \
+ -H "Content-Type: multipart/form-data" -H "Content-Type: application/json"  \
+ -F "jobName=$taskName" -F "jobDescription=$taskDescription" -F "automationFramework=$automationFramework" \
+ -F "projectKey=$projectKey" -F "versionName=$versionName" -F "cycleName=$cycleName" -F "createNewCycle=$createNewCycle" \
+ -F "appendDateTimeInCycleName=$appendDateTimeInCycleName" -F "folderName=$folderName" -F "createNewFolder=$createNewFolder" \
+ -F "appendDateTimeInFolderName=$appendDateTimeInFolderName" -F "assigneeUser=$assigneeUser" -F "mandatoryFields=$mandatoryFields" \
+ -F "file=$resultPath" -XPOST https://prod-vortexapi.zephyr4jiracloud.com/api/v1/automation/job/saveAndExecute
+result="$(cat  headers | head -n 1)"
+echo "Test results: \n"
+echo $result
+# END of the "Create task" code
 
-:: =========================================
-:: Update the properties of automation task and run it, send test results to Zephyr.
-::
-:: The code below uses "task properties" values defined in lines 38-56.
-:: Update them to change task properties.
-::
-:: To run the code below, first comment out the lines 62-63 above that create your automation task,
-:: and then uncomment in the lines below.
-:: =========================================
+#######################################################################
+#  Update the properties of automation task and  run it, send test results to Zephyr.
+#  
+#  The code below uses "task properties" values defined in lines 40-58.
+#  Update them to change task properties.
+#
+#  To run the code below, first comment out the code lines 64-68 that create your automation task, 
+#  and then uncomment in the lines below.
+#######################################################################
 
-:: Id of your automation task. You get it after the task is created.
-:: set "$taskId=XXXXXXXXXXXXXXXXXXXX42E96E8863BF7EEE44BDB95FF763DA3E9C3BC4776886"
+# Id of your automation task. You get it after the task is created.
+# taskId="XXXXXXXXXXXXXXXXXXXX42E96E8863BF7EEE44BDB95FF763DA3E9C3BC4776886"
 
-:: echo "Updating and running the automation task"
-:: curl -v -X PUT https://prod-vortexapi.zephyr4jiracloud.com/api/v1/automation/job/updateAndExecute -H "Content-Type: multipart/form-data" -H "Content-Type: application/json" -H "accessKey: %$accessKey%" -H "jwt: %$finaljwt%" -F "jobId=%$taskId%" -F "jobName=%$taskName%" -F "jobDescription=%$taskDescription%" -F "automationFramework=%$automationFramework%" -F "versionName=%$versionName%" -F "cycleName=%$cycleName%" -F "createNewCycle=%$createNewCycle%" -F "appendDateTimeInCycleName=%$appendDateTimeInCycleName%" -F "folderName=%$folderName%" -F "createNewFolder=%$createNewFolder%" -F "appendDateTimeInFolderName=%$appendDateTimeInFolderName%" -F "assigneeUser=%$assigneeUser%" -F "file=%$resultPath%"
+#echo "Update and run the automation task... "
+
+#curl -o headers -s -v -H "accessKey: $accessKey" -H "jwt: $jwt" -H "Content-Type: multipart/form-data" -H "Content-Type: application/json" -F "jobId=$taskId" -F "jobName=$taskName" -F "jobDescription=$taskDescription" -F "automationFramework=$automationFramework" -F "versionName=$versionName" -F "cycleName=$cycleName" -F "createNewCycle=$createNewCycle" -F "appendDateTimeInCycleName=$appendDateTimeInCycleName" -F "folderName=$folderName" -F "createNewFolder=$createNewFolder" -F "appendDateTimeInFolderName=$appendDateTimeInFolderName" -F "assigneeUser=$assigneeUser" -F "file=$resultPath" -XPUT https://prod-vortexapi.zephyr4jiracloud.com/api/v1/automation/job/updateAndExecute
+
+#result="$(cat  headers | head -n 1)"
+#echo $result
+
+# END of the "Update task" code
